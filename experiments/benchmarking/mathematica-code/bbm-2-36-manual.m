c98f825f58ceeeee63a40798959c3e3f0ad16c6c
lhydave <lhydave@163.com>
2025-07-28 23:07:03 +0800

    refactor: rename optmix function to optMix and improve comments for clarity

--- experiments/benchmarking/mathematica-code/bbm-2-36-manual.m
+++ experiments/benchmarking/mathematica-code/bbm-2-36-manual.m
@@ -38,16 +38,14 @@ constraint3 = {Root[1 - 2 # - #^2 + #^3& , 2, 0] <= g1 < 1,
    1 >= g1 >= g2 >= 0, a == 0, 0 <= b <= 1, 0 <= c <= 1 - g1, d == 0, 
    0 <= h2 <= 1, h2 == g2};
 
-(* an equivalent constraint for optimal mixing operation *)
-optmix[vara1_, varb1_, vara2_, varb2_] := Piecewise[{
-	{ Min[Min[Max[vara1, vara2], Max[varb1, varb2]], Max[(1 - (vara1 * varb2 - vara2 * varb1) / (vara1 + varb2 - vara2 - varb1)) * vara1 + (vara1 * varb2 - vara2 * varb1) / (vara1 + varb2 - vara2 - varb1) * varb1, (1 - (vara1 * varb2 - vara2 * varb1) / (vara1 + varb2 - vara2 - varb1)) * vara2 + (vara1 * varb2 - vara2 * varb1) / (vara1 + varb2 - vara2 - varb1) * varb2]], ((vara1 > varb1 && vara2 < varb2) || (vara1 < varb1 && vara2 > varb2)) }},
-	Min[Max[vara1, vara2], Max[varb1, varb2]]];
+(* constraint for optimal mixing operation *)
+optMix[ar_, br_, ac_, bc_] := If[ac <= ar && bc <= br, Min[ar, br], If[ac >= ar && bc >= br, Min[ac, bc], If[ar + bc - ac - br != 0, (ar*bc - ac*br)/(ar + bc - ac - br), Min[Max[ac, bc], Max[ar, br]]]]];
 
-bound = optmix[a, b, c, d];
+bound = optMix[a, b, c, d];
 
 (* solve the approximation bound *)
 branch1 = NMaxValue[{bound, constraint1}, {a, b, c, d, g1, g2, h2}, WorkingPrecision -> 20, AccuracyGoal -> 10, MaxIterations -> 2000];
-branch2 = NMaxValue[{If[1/3 < g1 < Root[1 - 2 # - #^2 + #^3& , 2, 0], Evaluate[bound], 0], constraint2}, {a, b, c, d, g1, g2, h2}, Method -> "RandomSearch",  WorkingPrecision -> 20, AccuracyGoal -> 10, MaxIterations -> 2000];
+branch2 = NMaxValue[{If[1/3 < g1 < Root[1 - 2 # - #^2 + #^3& , 2, 0], Evaluate[bound], 0], constraint2}, {a, b, c, d, g1, g2, h2}, WorkingPrecision -> 20, AccuracyGoal -> 10, MaxIterations -> 2000];
 branch3 = NMaxValue[{bound, constraint3}, {a, b, c, d, g1, g2, h2},  WorkingPrecision -> 20, AccuracyGoal -> 10, MaxIterations -> 2000];
 
 result = Max[branch1, branch2, branch3]
